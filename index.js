cd0401ab9c751d60ff5de27264289f9f33a52aac
Ivan Wills <ivan.wills@gmail.com>
2016-10-19 19:40:56 +1100

    Switched to plugin helper

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
+import { createFilter } from 'rollup-pluginutils';
 import Ractive from 'ractive';
 
-class Plug {
-    function transform (source) {
-        return Ractive.parse(source);
-    }
-};
+export default function ( options = {} ) {
+    var filter = createFilter( options.include, options.exclude );
 
-export default Plug;
+    return {
+        transform ( code, id ) {
+            if ( !filter( id ) ) return;
+
+            // proceed with the transformation...
+            return {
+                code: function transform (source) {
+                    return Ractive.parse(source);
+                },
+                //map: generatedSourceMap
+            };
+        }
+    };
+}
